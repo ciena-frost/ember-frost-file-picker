ca85089ecd3a5f94506694ac2aecef9838e24502
Seena <seenarowhani95@gmail.com>
2016-06-02 08:12:26 -0400

    promisify, added error handler

--- addon/components/frost-file-picker.js
+++ addon/components/frost-file-picker.js
@@ -1,6 +1,5 @@
 import Ember from 'ember'
 import layout from '../templates/components/frost-file-picker'
-import _ from 'lodash'
 
 const {Component, isNone, on, run} = Ember
 
@@ -8,7 +7,7 @@ const {Component, isNone, on, run} = Ember
 export default Component.extend({
   layout: layout,
   classNames: ['frost-file-picker'],
-  classNameBindings: ['fileName'],
+  classNameBindings: ['isDragging:over:'],
   accept: '*',
 
   initContext: on('didInitAttrs', function () {
@@ -19,44 +18,45 @@ export default Component.extend({
       }
     })
   }),
-
   bindChange: on('didInsertElement', function () {
     this.$('.frost-file-select').on('change', run.bind(this, 'filesSelected'))
   }),
-
   unbindChange: on('willDestroyElement', function () {
     this.$('.frost-file-select').off('change', run.bind(this, 'filesSelected'))
   }),
-
+  fireHandler: function (file) {
+    if (typeof this.attrs['onChange'] === 'function') {
+      this.attrs['onChange']({
+        id: this.get('id'),
+        type: 'file',
+        value: file
+      })
+    }
+  },
   filesSelected: function (files) {
     if (!(files instanceof window.FileList)) {
       files = files.target.files
     }
-    this.set('fileName', null)
-
-    if (typeof (this.attrs['validate']) === 'function') {
-      this.attrs['validate'](files[0]).then((result) => {
-        if (result.valid) {
-          this.set('fileName', files[0].name)
-          if (_.isFunction(this.attrs['onChange'])) {
-            this.attrs['onChange']({
-              id: this.get('id'),
-              type: 'file',
-              value: files[0]
-            })
+    files = [].slice.call(files)
+    let self = this
+    let name = files.reduce((e, r) => `${e}${r.name}, `, '')
+    this.set('fileName', name.slice(0, -2))
+    files.forEach((file) => {
+      new Promise(function (resolve, reject) {
+        if (typeof (self.attrs['validate']) === 'function') {
+          self.attrs['validate'](file).then((result) => {
+            result.valid ? resolve(file) : reject(result)
+          })
+        } else {
+          try {
+            resolve(file)
+          } catch (e) {
+            reject(e)
           }
         }
-      })
-    } else {
-      this.set('fileName', files[0].name)
-      if (_.isFunction(this.attrs['onChange'])) {
-        this.attrs['onChange']({
-          id: this.get('id'),
-          type: 'file',
-          value: files[0]
-        })
-      }
-    }
+      }).then(this.get('fireHandler').bind(this))
+        .catch(this.attrs['onError'])
+    })
   },
   click: function (event) {
     if (!this.$(event.target).hasClass('frost-file-select')) {
@@ -65,19 +65,19 @@ export default Component.extend({
   },
   dragOver: function (event) {
     event.preventDefault()
-    this.$('.background').addClass('over')
-  },
-  drop: function (event) {
-    event.preventDefault()
-    this.filesSelected(event.dataTransfer.files)
-    this.$('.background').removeClass('over')
+    this.set('isDragging', true)
   },
   dragEnter: function (event) {
     event.preventDefault()
-    this.$('.background').addClass('over')
+    this.set('isDragging', true)
   },
   dragLeave: function (event) {
     event.preventDefault()
-    this.$('.background').removeClass('over')
+    this.set('isDragging', false)
+  },
+  drop: function (event) {
+    event.preventDefault()
+    this.set('isDragging', false)
+    this.filesSelected(event.dataTransfer.files)
   }
 })
