a7fcddcb75c5df2f1296656ba34fdec95707cf5d
David Fortin <dafortin@ciena.com>
2016-09-22 10:08:57 -0400

    Increase to next major version

--- blueprints/ember-frost-file-picker/index.js
+++ blueprints/ember-frost-file-picker/index.js
@@ -5,7 +5,6 @@ module.exports = {
   /**
     Installs specified packages at the root level of the application.
     Triggered by 'ember install <addon name>'.
-
     @returns {Promise} package names and versions
   */
   afterInstall: function () {
