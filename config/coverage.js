4886d62f58d7efac52adcbb8ccca19d4a74ec587
Roxanne Panthaky <rpanthak@ciena.com>
2016-11-01 11:49:27 -0400

    get coverage reporting working

--- config/coverage.js
+++ config/coverage.js
@@ -1,6 +1,12 @@
 module.exports = {
+  coverageEnvVar: 'COVERAGE',
   excludes: [
-    '*/app/**/*',
     '*/tests/**/*'
+  ],
+  useBabelInstrumenter: true,
+  reporters: [
+    'html',
+    'lcov',
+    'text-summary'
   ]
 }
