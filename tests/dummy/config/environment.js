a6b898d89fd8cfdafcab755b46ca6b4f82efa595
Roxanne Panthaky <rpanthak@ciena.com>
2016-11-03 16:04:16 -0400

    fix demo page

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -39,7 +39,7 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
-    ENV.rootURL = '/frost-file-picker'
+    ENV.rootURL = '/file-picker'
     ENV.isDemo = true
   }
 
