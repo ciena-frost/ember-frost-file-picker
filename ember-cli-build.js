d08c66c606361ffa84c693dcb2395675153d32d6
Jeremy Brown <jeremy@notmessenger.com>
2017-08-11 09:05:18 -0500

    Refinement to previous commit

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,9 +6,6 @@ module.exports = function (defaults) {
     babel: {
       optional: ['es7.decorators']
     },
-    'ember-cli-babel': {
-      includePolyfill: true
-    },
     'ember-cli-mocha': {
       useLintTree: false
     },
