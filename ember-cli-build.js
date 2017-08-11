4b169426b5c92a464ae481850488ac15308a66c1
Jeremy Brown <jeremy@notmessenger.com>
2017-08-11 08:43:00 -0500

    Fix issuerelated to ciena-frost/ember-frost-core#488

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,6 +6,9 @@ module.exports = function (defaults) {
     babel: {
       optional: ['es7.decorators']
     },
+    'ember-cli-babel': {
+      includePolyfill: true
+    },
     'ember-cli-mocha': {
       useLintTree: false
     },
