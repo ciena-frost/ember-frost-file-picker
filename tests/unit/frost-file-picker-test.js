614d9412b871093e28fa2cf965f83b6b596351ff
Gabriel Knoy <gknoy@ciena.com>
2017-08-25 14:15:43 -0700

    Ensure that we always validate proptypes in at least one test

--- tests/unit/frost-file-picker-test.js
+++ tests/unit/frost-file-picker-test.js
@@ -1,7 +1,10 @@
 import {expect} from 'chai'
 import {setupComponentTest} from 'ember-mocha'
-import {helpers} from 'ember-prop-types'
-import {beforeEach, describe, it} from 'mocha'
+import {
+  helpers,
+  settings as propTypesSettings
+} from 'ember-prop-types/mixins/prop-types'
+import {afterEach, beforeEach, describe, it} from 'mocha'
 const {validatePropTypes} = helpers
 
 describe('Unit / Component / frost-file-picker /', function () {
@@ -13,10 +16,23 @@ describe('Unit / Component / frost-file-picker /', function () {
   })
 
   describe('`proptypes` validation', function () {
+    let throwErrors
+
+    beforeEach(function () {
+      // save our real settings so that we can force validation,
+      // in case our environment.js ever decides that such validation should be optional.
+      throwErrors = propTypesSettings.throwErrors
+      propTypesSettings.throwErrors = true
+    })
+
+    afterEach(function () {
+      propTypesSettings.throwErrors = throwErrors
+    })
+
     it('should have valid `proptypes` declaration', function () {
       expect(function () {
         validatePropTypes(component)
-      }).not.to.throw()
+      }).to.not.throw()
     })
   })
 })
