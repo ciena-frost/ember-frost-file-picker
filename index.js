676553b60acbcafe5ccaa6ea033837d1b8cbb14f
Michael Carroll <juwara0@users.noreply.github.com>
2017-12-06 11:56:23 -0600

    remove loading of babel configuration for unused ember-computed-decorators

--- index.js
+++ index.js
@@ -3,17 +3,5 @@
 'use strict'
 
 module.exports = {
-  name: 'ember-frost-file-picker',
-
-  init: function () {
-    this.options = this.options || {}
-    this.options.babel = this.options.babel || {}
-    this.options.babel.optional = this.options.babel.optional || []
-
-    if (this.options.babel.optional.indexOf('es7.decorators') === -1) {
-      this.options.babel.optional.push('es7.decorators')
-    }
-    // eslint-disable-next-line no-unused-expressions
-    this._super.init && this._super.init.apply(this, arguments)
-  }
+  name: 'ember-frost-file-picker'
 }
